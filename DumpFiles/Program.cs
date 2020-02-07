8158710ab4bd100a410038308a1481ec712deabc
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-02-07 16:55:49 -0500

    initial commit and project structure

---
+++ DumpFiles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using DumpFiles.Utils;
+
+namespace DumpFiles
+{
+    using static DirectoryInfoUtils;
+
+    public class Storage
+    {
+
+    }
+
+    /// <summary>
+    ///     Programmatically embed resources in csharp dll
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin process...");
+
+            // TODO: Get content file path from the provided arguments.
+            var currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            // Get project file
+            var csprojRegex =new Regex("\\.csproj"); 
+            var currentProjectFile =
+                currentDirectory.GetFullnameForFileInHigherDirectory(csprojRegex, "*.csproj");
+
+            // - OR - 
+            // var currentProjectFile = currentDirectory.GetCurrentProjectFile();
+            Console.WriteLine(currentProjectFile);
+
+            // Read and enhance project file.
+            // TODO
+            // TODO
+            // TODO
+
+            // Run csharp compiler.
+            var compiler = new Standard("C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe");
+            // compiler.RunAndWaitForExit("");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DumpFiles/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DumpFiles.Utils
+{
+    public static class DirectoryInfoUtils
+    {
+        public static bool DirectoryContains(this DirectoryInfo directory, Regex searchRegex)
+        {
+            var dirFiles = directory
+                .GetFiles("*", SearchOption.TopDirectoryOnly)
+                .ToList()
+                .Select(x => x.ToString());
+
+            return dirFiles.Any(x => searchRegex.IsMatch(x));
+        }
+
+        public static string GetParentOfFileInHigherDirectory(this DirectoryInfo directory, Regex searchRegex)
+        {
+            var i = 0;
+            while(!directory.DirectoryContains(searchRegex))
+            {
+                directory = directory.Parent;
+
+                // Throw argument exception
+                i++;
+                if (i == 10) { throw new ArgumentException(); }
+            }
+
+            return directory.ToString();
+        }
+
+        public static string GetFullnameForFileInHigherDirectory(
+            this DirectoryInfo directory,
+            Regex searchRegex,
+            string searchText)
+        {
+            var parent = directory.GetParentOfFileInHigherDirectory(searchRegex);
+
+            return Directory.GetFiles(parent.ToString(), searchText)[0];
+        }
+
+        public static string GetCurrentProjectFile(this DirectoryInfo directory)
+        {
+            var projectFileRegex = new Regex("\\.csproj$");
+
+            var parent = GetParentOfFileInHigherDirectory(directory, projectFileRegex);
+
+            // Get project file name
+            var matches = Directory.GetFiles(parent.ToString(), "*.csproj");
+
+            return matches[0];;
+        }
+    }
+}
